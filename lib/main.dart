7c08dd74d7031e29229cfa480e0b599826944064
Bui Khac Lam <lamkhac23102001@gmail.com>
2022-09-25 14:49:35 +0700

    [add] full authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:rider_application/global/global.dart';
+import 'package:rider_application/splashScreen/splash_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  sharedPreferences = await SharedPreferences.getInstance();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -11,11 +18,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Riders App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(),
+      home: MySplashScreen(),
     );
   }
 }
